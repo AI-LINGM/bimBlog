8773eaba30d89ff2e1b2350f03e701d1eadb9e77
Ai-Ling <ailing.gonzalezmac@gmail.com>
2024-05-14 09:55:24 -0400

    feat: Add: - Author - Category - Comment - BlogPost Make first Migration and updated views and README.MD

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from blog.models import Author, Category, Comment, BlogPost
+
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(BlogPost)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from . import models
+
 
 def home(request):
-    return render(request, "blog/index.html")
+    posts = models.BlogPost.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, "blog/index.html", context)
