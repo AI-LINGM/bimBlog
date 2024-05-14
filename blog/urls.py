bb494926efb722594b898e456a026aedd6abacd3
Ai-Ling <ailing.gonzalezmac@gmail.com>
2024-05-14 17:06:08 -0400

    feat: Add Search logic.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from blog.views import home, blog_post_detail, create_blog_post
+from blog.views import home, blog_post_detail, create_blog_post, search
 
 urlpatterns = [
     path('', home, name='home'),
     path('post/details/<int:pk>', blog_post_detail, name='post_details'),
     path('create_post/', create_blog_post, name='create_post'),
+    path('search/', search, name='search'),
 ]
